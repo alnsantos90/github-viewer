b32acb997a10b17620555c790365c8be8c08ddc0
Allan Santos <allansantos@Allans-iMac-Pro.local>
2021-01-11 23:13:39 -0300

    Updates

---
+++ precache-manifest.f21f7692430ce8afca59b0d96088970b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50e71978f0a5fef125f98b1e33928a7",
+    "url": "/github-viewer/index.html"
+  },
+  {
+    "revision": "7a19140da7841cf9736d",
+    "url": "/github-viewer/static/css/main.e5f3f58f.chunk.css"
+  },
+  {
+    "revision": "f65300802677a127cc95",
+    "url": "/github-viewer/static/js/2.1d58f25e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-viewer/static/js/2.1d58f25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a19140da7841cf9736d",
+    "url": "/github-viewer/static/js/main.658c5948.chunk.js"
+  },
+  {
+    "revision": "4de9493d44a721446240",
+    "url": "/github-viewer/static/js/runtime-main.4ba0188c.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/github-viewer/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/github-viewer/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "c4badf757d1d54b39021fabcad563625",
+    "url": "/github-viewer/static/media/folha1.c4badf75.png"
+  },
+  {
+    "revision": "337732903b8bdf7f69c9f92840b2d86e",
+    "url": "/github-viewer/static/media/folha2.33773290.png"
+  },
+  {
+    "revision": "7a788c08017d87c70115ddd9a7e4c0b8",
+    "url": "/github-viewer/static/media/folha3.7a788c08.png"
+  },
+  {
+    "revision": "e7afb0f606513967a6d6132a99267437",
+    "url": "/github-viewer/static/media/folha4.e7afb0f6.png"
+  }
+]);
